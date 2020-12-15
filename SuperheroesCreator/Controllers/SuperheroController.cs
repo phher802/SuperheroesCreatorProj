c12fe5e5493350d54f3e5254c8d2aa494fd65d54
phher802 <pher024@gmail.com>
2020-12-14 21:38:41 -0600

    Create member variable and constructor to instantiate db

---
+++ SuperheroesCreator/Controllers/SuperheroController.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperheroesCreator.Data;
+using SuperheroesCreator.Models;
+
+namespace SuperheroesCreator.Controllers
+{
+    public class SuperheroController : Controller
+    {   //in order to add to data table, create private member variable and constructor
+        private ApplicationDbContext _context;
+        //constructor is using built-in dependency injection to inject applicationDBcontext
+        //into this constroller to be used
+        public SuperheroController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        // GET: SuperheroController
+        public ActionResult Index()
+        {
+           
+        }
+
+        // GET: SuperheroController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: SuperheroController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SuperheroController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: SuperheroController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
