794dbf6388b351b33090f2d297db5c0f8e98508f
phher802 <pher024@gmail.com>
2020-12-14 19:59:12 -0600

    Init

---
+++ SuperheroesCreator/Models/Superhero.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperheroesCreator.Models
+{
+    public class Superhero
+    {
+    }
+}
